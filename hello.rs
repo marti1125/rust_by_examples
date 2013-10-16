5a8955dacdc9930ae3df4ab30b970e08d0e1773c
Willy Aguirre <marti1125@gmail.com>
2013-10-15 22:19:40 -0500

    corrections

--- hello.rs
+++ hello.rs
@@ -1,3 +1,3 @@
 fn main() {
-    io::println("hello?");
+    println("Hello, world.");
 }
---
+++ testing.rs
@@ -0,0 +1,10 @@
+#[test]
+fn this_tests_code() {
+	// Run the test: $ rust test testing.rs
+	println("testing....")
+}
+
+#[test]
+fn this_tests_code() {
+	fail!("Fail!");
+}
